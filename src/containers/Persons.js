1777724f3d48f1644fc8900896b793c9a8c6f768
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-16 00:28:08 +0200

    Use the state and the dispatch methods in the Persons Component

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -6,10 +6,6 @@ import AddPerson from '../components/AddPerson/AddPerson';
 import * as ActionTypes from '../store/actions';
 
 class Persons extends Component {
-  state = {
-    persons: []
-  }
-
   personAddedHandler = () => {
     const newPerson = {
       id: Math.random(), // not really unique but good enough here!
@@ -30,13 +26,13 @@ class Persons extends Component {
   render() {
     return (
       <div>
-        <AddPerson personAdded={this.personAddedHandler} />
-        {this.state.persons.map(person => (
+        <AddPerson personAdded={this.props.onAddPerson} />
+        {this.props.persons.map(person => (
           <Person
             key={person.id}
             name={person.name}
             age={person.age}
-            clicked={() => this.personDeletedHandler(person.id)} />
+            clicked={() => this.props.onDeletePerson(person.id)} />
         ))}
       </div>
     );
@@ -52,7 +48,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     onAddPerson: () => dispatch({type: ActionTypes.ADD_PERSON}),
-    onDeletePerson: () => dispatch({type: ActionTypes.DELETE_PERSON})
+    onDeletePerson: (personId) => dispatch({type: ActionTypes.DELETE_PERSON, id: personId})
   }
 }
 
